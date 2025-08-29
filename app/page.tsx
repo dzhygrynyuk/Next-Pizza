bc3e9f51568a96e6cd7cd06de595d2688aeb661d
Vasyl Dzhyhrynyuk <dzhygrynyuk@gmail.com>
2025-08-29 21:46:35 +0300

    Gategories and SortPopup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { Categories, Container, SortPopup, Title } from "@/components/shared";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1>Main</h1>
+    <>
+      <Container className="mt-10">
+        <Title text="All pizzas" size="lg" className="font-extrabold" />
+        <Categories />
+        <SortPopup />
+      </Container>
+    </>
   );
 }
